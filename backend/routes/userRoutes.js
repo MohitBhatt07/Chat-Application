9f4d045182b08ba85a8af32173af9818b27c17ac
Mohit Bhatt <bhattmohit869@gmail.com>
2023-10-15 14:55:13 +0530

    added login controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const {registerUser} = require("../controllers/userControllers");
+const {authUser} = require('../controllers/userControllers');
 const router = express.Router();
 
-// router.route('/').post(registerUser);
-// router.route('/login').post(loginUser);
+router.route('/').post(registerUser);
+router.route('/login').post(authUser);
 
 module.exports = router; 
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const app = express();
 const cors = require('cors');
 const connectDB = require("./config/db");
-const router = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes")
 
 app.use(cors());
 app.use(express.json());
@@ -14,14 +14,7 @@ app.get("/", (req, res) => {
   return res.send(`Api is running`);       
 });   
 
-app.get('/api/chat',(req,res)=>{
-  return res.send(chatData);
-})
-
-app.get('/api/chat/:id',(req,res)=>{
-  const singleChat = chatData.find((chat)=>chat.id === req.params.id);
-  return res.send(singleChat);
-})
+app.use('/api/user' , userRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port,()=>console.log(`server started at localhost:${port}`));
